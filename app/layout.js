450ed0d73d2b5b06bfd01151cec8447cb8a06bd0
kasundraJeet <kasundrajeet29@gmail.com>
2024-08-27 05:39:17 +0530

    set layout for charka ui

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,11 @@
-import { Inter } from "next/font/google";
+import { Rubik } from 'next/font/google'
+import { Providers } from "@/components/provider";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const rubik = Rubik({
+  subsets: ['latin'],
+  variable: '--font-rubik',
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={rubik.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
