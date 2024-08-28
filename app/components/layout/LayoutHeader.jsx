7e33cb7d605dab37b4f907c27f714afd42ffaa4a
kasundraJeet <kasundrajeet29@gmail.com>
2024-08-28 10:52:10 +0530

    set user session detail

--- app/components/layout/LayoutHeader.jsx
+++ app/components/layout/LayoutHeader.jsx
@@ -1,17 +1,78 @@
-import { Box, Container, Flex , Avatar, Button } from "@chakra-ui/react";
-import { useSession } from "next-auth/react";
+import {
+  Box,
+  Container,
+  Flex,
+  Avatar,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  HStack,
+  Text,
+  Stack,
+} from "@chakra-ui/react";
+import { useSession , signOut} from "next-auth/react";
 
 export default function Header() {
   const { data: session } = useSession();
   return (
     <Box as="header" width="100%" py={2}>
-      <Container maxW="4xl">
+      <Container maxW="2xl">
         <Flex justifyContent="space-between" alignItems="center">
           <h2>Jeet Kasundra</h2>
-          {session ?
-            <Avatar size='sm'  name={session.user?.name} src={session.user?.image} /> : <Button colorScheme="gray">Login</Button>}
+          {session ? (
+            <HStack spacing={3}>
+              <Text fontSize="md">{session.user?.email}</Text>
+              <Menu>
+                <MenuButton>
+                  <Avatar
+                    size="sm"
+                    name={session.user?.name}
+                    src={session.user?.image}
+                  />
+                </MenuButton>
+                <MenuList marginTop={1}>
+                  <MenuItem>
+                    <Stack spacing={0}>
+                        <Text fontSize="sm" color="black">{session.user?.name}</Text>
+                        <Text fontSize="xs" color="gray">{session.user?.email}</Text>
+                    </Stack>
+                  </MenuItem>
+                  <MenuItem color="red" icon={<LogOutIcon />} onClick={() => signOut()}>
+                    Log Out
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+            </HStack>
+          ) : (
+            <Button as="a" href="/login" colorScheme="gray">
+              Login
+            </Button>
+          )}
         </Flex>
       </Container>
     </Box>
+  );
+}
+
+function LogOutIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="lucide lucide-log-out"
+    >
+      <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+      <polyline points="16 17 21 12 16 7" />
+      <line x1="21" x2="9" y1="12" y2="12" />
+    </svg>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,10 +1,36 @@
-'use client'
-import {LayoutWrapper} from "@/components/layout"
+"use client";
+import { LayoutWrapper } from "@/components/layout";
+import { Box, Container, Stack, Text } from "@chakra-ui/react";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession();
+  console.log(session)
   return (
     <LayoutWrapper>
-
+      <Container maxWidth="2xl">
+        <Box py={8} px={2}>
+          <Stack spacing={8}>
+            <Stack spacing={3}>
+              <Text as="h1" fontWeight="600" fontSize="x-large">
+                NextAuth.js Example (Server Side)
+              </Text>
+              {session ? (
+                <Text as="h2" fontSize="md">
+                  Hello, {session.user?.name}! You have successfully logged in.
+                </Text>
+              ) : (
+                <Box py={2}>
+                  <Text as="h2" fontSize="lg" color="red.500">
+                    You are not logged in. Please log in to continue.
+                  </Text>
+                </Box>
+              )}
+            </Stack>
+            {session ?  <pre>{JSON.stringify(session, null, 2)}</pre> : null }
+          </Stack>
+        </Box>
+      </Container>
     </LayoutWrapper>
   );
 }
